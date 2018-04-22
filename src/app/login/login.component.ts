ac0c916794b19b9a389c96ed8813ed379f8ac996
Franklin Basa <basa.franklin@yahoo.com>
2018-04-22 11:37:23 +0800

    inject authservice to login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../route.animations'
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -13,7 +14,11 @@ export class LoginComponent implements OnInit {
 
   user:any;
   form: FormGroup;
-  constructor(private router: Router,private formBuilder: FormBuilder,) { 
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private auth: AuthService
+  ) { 
 
     this.form = formBuilder.group({
       name: ['', Validators.required],

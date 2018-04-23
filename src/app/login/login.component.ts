27b707555c74d1f5078835ab8ae980f74dab99de
Franklin Basa <basa.franklin@yahoo.com>
2018-04-23 12:17:41 +0800

    commented out authservice

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,7 @@ import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../route.animations'
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AuthService } from '../shared/services/auth.service';
+//import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -17,7 +17,7 @@ export class LoginComponent implements OnInit {
   constructor(
     private router: Router,
     private formBuilder: FormBuilder,
-    private auth: AuthService
+    //private auth: AuthService
   ) { 
 
     this.form = formBuilder.group({
